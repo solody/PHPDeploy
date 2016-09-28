ddf2bb775115595bd0cfa19dafe3cdb3a55d4527
yangshinan <yangsn@figo.cn>
2016-09-28 14:03:05 +0800

    解决wget和composer没有标准输出的问题，wget加上无限重试和续传

--- composer_install.sh
+++ composer_install.sh
@@ -5,4 +5,4 @@ all_version=($(ls ./deploy/$1 -t))
 last_version=${all_version[0]}
 
 cd ./deploy/$1/${last_version}
-composer install -vvv
+composer install -vvv 2>&1
--- download.sh
+++ download.sh
@@ -16,6 +16,6 @@ if [ ! -d "./deploy/$1" ]; then
   mkdir ./deploy/$1
 fi
 
-wget --http-user=$4 --http-passwd=$5 -P ./package/$1 $2$3.tar.gz
+wget --http-user=$4 --http-passwd=$5 -c -t0 -P ./package/$1 $2$3.tar.gz 2>&1
 
 tar zxvf ./package/$1/$3.tar.gz -C ./deploy/$1
